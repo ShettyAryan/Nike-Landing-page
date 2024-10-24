bf8fff01659d7b4230f7dc8c222afecf664ad2f3
Aryan Shetty <shetty.aryan1923@gmail.com>
2024-10-25 00:15:50 +0530

    Updated Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,47 @@
-import{
-  Hero,
-  PopularProducts,
-  SuperQuality,
-  CustomerReviews,
-  Footer,
-  Services,
-  SpecialOffer,
-  Subscribe
+import Hero from './sections/Hero';
+import Footer from './sections/Footer';
+import CustomerReviews from './sections/CustomerReviews';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import Subscribe from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+import Popular from './sections/Popular';
+import Nav from './Components/Nav';
 
-} from './sections';
-import Nav from './components/Nav';
+const App =()=>(
+<main className="relative">
+   <Nav /> 
+  <section className="xl:padding-1 wide:padding-r padding-b">
+    <Hero />
+  </section>
+  <section className="padding">
+    <Popular />
 
-const App = () => (
-  <main className="relative">
-    <Nav/>
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero/>
-    </section>
-    <section className="padding">
-     <PopularProducts/>  
-    </section>
-    <section className="padding">
-     <SuperQuality/>  
-    </section>
-    <section className="padding">
-     <Services/> 
-    </section>
-    <section className="padding-x py-10">
-     <SpecialOffer/>  
-    </section>
-    <section className="bg-pale-blue padding">
-     <CustomerReview/>  
-    </section>
-    <section className="padding-x sm:py-32 py-16 w-full ">
-     <Subscribe/> 
-    </section>
-    <section className="bg-black padding-x padding-t pb-8">
-     <Footer/> 
-    </section>
+  </section>
+  <section className="padding">
+    <SuperQuality />
 
-  </main>
+  </section>
+  <section className="padding-x py-10">
+   <Services />
 
+  </section>
+  <section className="padding">
+    <SpecialOffer />
+
+  </section>
+  <section className="bg-pale-blue padding">
+    <CustomerReviews />
+
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    <Subscribe />
+
+  </section>
+  <section className="bg-black padding-x padding-t pb-8">
+    <Footer />
+
+  </section>
+</main>
 );
-
 export default App;
